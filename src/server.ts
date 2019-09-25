c3e155caaa822ec1f22b6700b1bccae3d0a1a53e
Yashwanth <yashwanth@seed12-2.local>
2019-09-25 18:23:47 +0530

    mongo connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 import {Request,Response} from 'express';
+import * as mongo  from 'mongodb';
 let app = express();
 let port = 2000;
+let url = 'mongodb://localhost/emp';
+
+mongo.connect(url,{useNewUrlParser: true,useUnifiedTopology: true}).then(() => { 
+    console.log('connected to MongoDB');
+},
+).catch(err => {
+    console.log("MongoDB connection error." + err);
+});
 
 app.get('/',(req: Request,res: Response) => {
     res.send(`App Listening on port : ${port}`);
