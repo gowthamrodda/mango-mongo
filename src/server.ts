3e17417875994d59fb973bbeb784a53f1df4401b
Yashwanth <yashwanth@seed12-2.local>
2019-09-25 12:13:19 +0530

    “express-app”

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express from 'express';
+import {Request,Response} from 'express';
+let app = express();
+let port = 2000;
+
+app.get('/',(req: Request,res: Response) => {
+    res.send(`App Listening on port : ${port}`);
+    
+})
+
+app.listen(port);
+
